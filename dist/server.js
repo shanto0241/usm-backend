d023b18f4ea94e2c7b1ce2752279c55249c2c620
shanto0241 <shantonu2050@gmail.com>
2024-05-22 21:31:54 +0600

    export

--- dist/server.js
+++ dist/server.js
@@ -12,10 +12,44 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const globalErrorHandler_1 = __importDefault(require("./apps/middlewares/globalErrorHandler"));
+const routers_1 = __importDefault(require("./apps/routes/routers"));
+const http_status_1 = __importDefault(require("http-status"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const mongoose_1 = __importDefault(require("mongoose"));
 const index_1 = __importDefault(require("./config/index"));
-const app_1 = __importDefault(require("./app"));
-process.on('uncaughtException', err => {
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+// parser
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cookie_parser_1.default)());
+// Application routes
+app.use('/api/v1', routers_1.default);
+// Root api
+app.get('/', (req, res) => {
+    res.send('Hello from university server! ');
+});
+//global error handler
+app.use(globalErrorHandler_1.default);
+//handle Not Found Route
+app.use((req, res, next) => {
+    res.status(http_status_1.default.NOT_FOUND).json({
+        success: false,
+        message: 'Not Found  does not exist ‼️',
+        errorMessages: [
+            {
+                path: req.originalUrl, //showing notfound route
+                message: 'API Not Found',
+            },
+        ],
+    });
+    next();
+});
+// Process termination handling
+process.on('uncaughtException', (err) => {
     console.log(err);
     process.exit(1);
 });
@@ -25,17 +59,15 @@ function server() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             yield mongoose_1.default.connect(index_1.default.database_url);
-            // infoconsole.log('♻️  Database connected✅');
-            console.log(`♻️  Database is connected successfully✌️`);
-            serverStatus = app_1.default.listen(index_1.default.port, () => {
-                // infoconsole.log(`Application app listening on port ${config.port}`);
+            console.log(`♻️ Database is connected successfully✌️`);
+            serverStatus = app.listen(index_1.default.port, () => {
                 console.log(`Application app listening on port ${index_1.default.port}`);
             });
         }
         catch (err) {
-            console.log('🙇‍♂️👎Failed connect to database', err);
+            console.log('‍♂️Failed connect to database', err);
         }
-        process.on('unhandleRejection', error => {
+        process.on('unhandledRejection', (error) => {
             if (serverStatus) {
                 serverStatus.close(() => {
                     console.log('Server closed ', error);
@@ -55,3 +87,4 @@ process.on('SIGTERM', () => {
         serverStatus.close();
     }
 });
+exports.default = app; // Export the app instance for potential external usage
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,51 @@
+import express, { Application, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './apps/middlewares/globalErrorHandler';
+import routers from './apps/routes/routers';
+import httpStatus from 'http-status';
+import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
 import config from './config/index';
-import app from './app';
 import { Server } from 'http';
 
-process.on('uncaughtException', err => {
+const app: Application = express();
+
+app.use(cors());
+
+// parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+// Application routes
+app.use('/api/v1', routers);
+
+// Root api
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello from university server! ');
+});
+
+//global error handler
+app.use(globalErrorHandler);
+
+//handle Not Found Route
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: 'Not Found  does not exist ‼️',
+    errorMessages: [
+      {
+        path: req.originalUrl, //showing notfound route
+        message: 'API Not Found',
+      },
+    ],
+  });
+  next();
+});
+
+// Process termination handling
+process.on('uncaughtException', (err) => {
   console.log(err);
   process.exit(1);
 });
@@ -14,18 +56,16 @@ let serverStatus: Server;
 async function server() {
   try {
     await mongoose.connect(config.database_url as string);
-    // infoconsole.log('♻️  Database connected✅');
-    console.log(`♻️  Database is connected successfully✌️`);
+    console.log(`♻️ Database is connected successfully✌️`);
 
     serverStatus = app.listen(config.port, () => {
-      // infoconsole.log(`Application app listening on port ${config.port}`);
       console.log(`Application app listening on port ${config.port}`);
     });
   } catch (err) {
-    console.log('🙇‍♂️👎Failed connect to database', err);
+    console.log('‍♂️Failed connect to database', err);
   }
 
-  process.on('unhandleRejection', error => {
+  process.on('unhandledRejection', (error) => {
     if (serverStatus) {
       serverStatus.close(() => {
         console.log('Server closed ', error);
@@ -36,10 +76,14 @@ async function server() {
     }
   });
 }
+
 server();
+
 process.on('SIGTERM', () => {
   console.log('SIGTERM is received');
   if (serverStatus) {
     serverStatus.close();
   }
 });
+
+export default app; // Export the app instance for potential external usage
